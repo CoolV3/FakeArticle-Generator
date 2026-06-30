4a2f13632f1d8fe07b480cef9ec2c7e32c2ec736
Constantin Meier <166921281+CoolV3@users.noreply.github.com>
2026-06-30 06:49:16 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-white">
+      <body className="min-h-full flex flex-col bg-white text-black">
       <div className="justify-center gap-5 w-full flex flex-col ">
         <div className="z-100 shrink-0">
           <NavbarComponent/>
